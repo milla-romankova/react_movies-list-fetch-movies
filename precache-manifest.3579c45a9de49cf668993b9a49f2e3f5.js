9409ff72c4e5e4bb67c5be11e50f228442b20924
milla-romankova <milla-romankova@gmail.com>
2020-11-10 22:37:26 +0200

    Updates

---
+++ precache-manifest.3579c45a9de49cf668993b9a49f2e3f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa843e041960f538aaa79da1f3c8b69",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4bdd81935bdf5a384531",
+    "url": "./static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "7dbba183000cd935ac06",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "4bdd81935bdf5a384531",
+    "url": "./static/js/2.aafbd298.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.aafbd298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dbba183000cd935ac06",
+    "url": "./static/js/main.f340966e.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
